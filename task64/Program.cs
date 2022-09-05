cbdcde7d72cabf3c6468f0ae9cb86e9a459c2074
OlegSam2 <doorsmania@mail.ru>
2022-09-05 19:08:02 +0300

    reverse order

---
+++ task64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+PrintNumbersToN(N);
+
+void PrintNumbersToN(int n)
+{
+    if (n < 1) return;      // условие остановки
+    Console.Write(n + " ");
+    PrintNumbersToN(n - 1);
+}
